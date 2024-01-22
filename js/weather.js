5aa613047673069c01f8832d1a655edeac5f76c3
song9537 <140922961+song9537@users.noreply.github.com>
2024-01-21 21:54:56 -0400

    Completed weather info

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+const API_key = "30c5a25b9c0646d8c5a09dc0735e7ecd";
+
 function onGeoSuccess(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log(lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = data.weather[0].main;
+    });
 }
 function onGeoError() {
   alert("Can't find you. No weather available.");
